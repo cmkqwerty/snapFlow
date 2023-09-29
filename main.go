5981aae23604329bdb8c821c6cad565692702bb3
cmkqwerty <56502922+cmkqwerty@users.noreply.github.com>
2023-09-29 21:02:28 +0300

    [feat] refactored contactHandler to template

--- main.go
+++ main.go
@@ -9,13 +9,12 @@ import (
 	"path/filepath"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func executeTemplate(w http.ResponseWriter, filepath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tplPath := filepath.Join("templates", "home.gohtml")
-	tpl, err := template.ParseFiles(tplPath)
+	tpl, err := template.ParseFiles(filepath)
 	if err != nil {
-		log.Printf("parsing template: %v", err)
-		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		log.Printf("processing template: %v", err)
+		http.Error(w, "There was an error processing the template.", http.StatusInternalServerError)
 		return
 	}
 
@@ -27,9 +26,14 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tplPath)
+}
+
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch email me at <a href=\"mailto:cemeke10@gmail.com\">cemeke10@gmail.com</a></p>")
+	tplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
