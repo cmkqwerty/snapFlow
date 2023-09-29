9db2584e2c947f252df69727299655ddb2592408
cmkqwerty <56502922+cmkqwerty@users.noreply.github.com>
2023-09-29 21:04:13 +0300

    [feat] refactored faqHandler to template

--- main.go
+++ main.go
@@ -37,16 +37,8 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, `<h1>FAQ Page</h1>
-<ul>
-	<li><b>Is there a free version?</b> Yes! We offer a free trial for 30 days on any paid plans.</li>
-	<li><b>What happens after my free trial?</b> If you chose to keep your paid plan after the 30 day trial, you will need to enter your payment details and confirm.</li>
-	<li><b>Can I upgrade or downgrade anytime?</b> Yes! SnapFlow is a pay-as-you-go service and you can upgrade, downgrade or cancel at any time.</li>
-	<li><b>What are your support hours?</b> We have support staff answering emails 24/7, though response times may be a bit slower on weekends.</li>
-	<li><b>How do I contact support?</b> Email us - <a href="mailto:support@snapflow.com">support@snapflow.com</a></li>
-</ul>
-`)
+	tplPath := filepath.Join("templates", "faq.gohtml")
+	executeTemplate(w, tplPath)
 }
 
 func main() {
