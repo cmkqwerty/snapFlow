a81caa070cfb957d2c2ad03faaa35d5eaa5e4de4
cmkqwerty <56502922+cmkqwerty@users.noreply.github.com>
2023-10-19 21:42:43 +0300

    [feat] add new gallery handler

---
+++ controllers/galleries.go
@@ -0,0 +1,23 @@
+package controllers
+
+import (
+	"github.com/cmkqwerty/snapFlow/models"
+	"net/http"
+)
+
+type Galleries struct {
+	Templates struct {
+		New Template
+	}
+	GalleryService *models.GalleryService
+}
+
+func (g Galleries) New(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		Title string
+	}
+
+	data.Title = r.FormValue("title")
+
+	g.Templates.New.Execute(w, r, data)
+}
